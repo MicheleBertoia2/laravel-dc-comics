e260d85980ad9c28ad46ea0f63324d7a572c06b1
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-06-12 17:07:58 +0200

    [add]  main layout  and  resource controller for comics

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        <h1  class="my-5">Comics selection</h1>
+
+    </div>
+
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@extends('layouts.main')
 
-    @vite('resources/js/app.js')
+@section('content')
+    <div class="container">
+        <h1  class="my-5">Home</h1>
 
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deleniti, odit suscipit quaerat atque deserunt neque. Id, sequi delectus! Consequuntur, sapiente. Impedit commodi amet laudantium soluta, reprehenderit dolorem! Voluptatibus minus alias in cupiditate doloribus enim eligendi voluptates saepe consectetur adipisci nihil, sed placeat rerum ullam illo exercitationem. Temporibus aliquam veniam fugit dolore perferendis cumque obcaecati! Obcaecati facere voluptatum atque magnam consectetur doloremque minus, officia minima porro ut distinctio numquam deleniti ducimus quisquam totam magni nihil, autem perferendis adipisci quos quaerat dolores possimus. Consectetur eaque doloribus assumenda consequatur, eligendi corrupti. Nemo, hic quisquam, dolores ipsam iste maxime corrupti numquam provident minus at aut tempora possimus fugit enim nam explicabo dolor. Debitis neque id ipsum perferendis similique dolores officia eaque beatae. Assumenda commodi ut illo esse consequatur iste hic dolor accusantium exercitationem asperiores, ea qui fuga facilis illum, corrupti tempore ducimus atque sapiente architecto repellendus cum! Illo non aspernatur deserunt animi, corrupti quia similique veritatis mollitia, sequi, voluptates hic odit at distinctio neque cumque impedit explicabo eos dolor molestiae molestias est aperiam inventore possimus ipsum! Consequatur, sit molestiae doloribus explicabo ut exercitationem perspiciatis natus reprehenderit in reiciendis dolore ullam neque aut eveniet optio ab odio temporibus nulla sapiente quibusdam error delectus harum!</p>
 
-    <title>Dc comics dashboard</title>
-</head>
-<body>
+    </div>
 
-    <h1>Comics selection</h1>
-</body>
-</html>
+@endsection
