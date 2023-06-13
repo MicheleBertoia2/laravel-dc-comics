62ded1055f82960cd30dd8abd87a50ad5a62862a
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-06-13 18:16:21 +0200

    [add] possibility to  eliminate comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,6 +5,14 @@
     <div class="container">
         <h1  class="my-5">Comics selection</h1>
 
+        @if (session('deleted'))
+
+            <div class="alert alert-success" role="alert">
+                {{session('deleted')}}
+            </div>
+
+        @endif
+
         <table class="table">
             <thead>
               <tr>
@@ -12,7 +20,7 @@
                 <th scope="col">Titolo</th>
                 <th scope="col">prezzo</th>
                 <th scope="col">Data pubblicazione</th>
-                <th scope="col"></th>
+                <th scope="col">Azioni</th>
               </tr>
             </thead>
             <tbody>
@@ -23,8 +31,20 @@
                         <td>{{$comic->title}}</td>
                         <td>{{$comic->price}}</td>
                         <td>{{$comic->sale_date}}</td>
-                        <td><a href="{{route('comics.show', $comic)}}" class="btn btn-success">Vai</a></td>
+                        <td><a href="{{route('comics.show', $comic)}}" class="btn btn-success" >Vai</a></td>
                         <td><a href="{{route('comics.edit', $comic)}}" class="btn btn-warning">Modifica</a></td>
+                        <td>
+                            <form
+                              action="{{route('comics.destroy',  $comic)}}"
+                              method="POST"
+                              class="d-inline"
+
+                              >
+                                @csrf
+                                @method("DELETE")
+                                <button type="submit" class="btn btn-danger">Elimina</button>
+                            </form>
+                        </td>
 
                     </tr>
 
